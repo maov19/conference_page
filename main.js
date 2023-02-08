f061a512b20b996e15fe4ecfa31a9a3ba426a965
mossa <martin.ossa.8@gmail.com>
2023-02-08 12:12:14 +0100

    Add loop to dynamic section display

--- main.js
+++ main.js
@@ -15,66 +15,75 @@ document.onclick = function closeMenu(clickEvent) {
   }
 };
 
-// 1.append a section to the speakers section - done
-// 2.display the inner html - done
-// 3.create objects with speakers info - done
-// 4.loop through objects
+// artists dynamic display
 
-const artist = document.getElementById('artists').innerHTML = `
-<div class="artist">
-<img class="artist_photo" src="images/james.png" alt="metallica logo">
-<div class="artist_info">
-    <h3 class="artist_name">Metallica</h3>
-    <p class="artist_story">Lorem ipsum dolor sit amet.</p>
-    <div class="line_left"></div>
-    <p class="artist_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.</p>
-</div>
-</div>
-`
+  // 0.create a sample artist display
+  // 1.create objects with speakers info - done
+  // 2.append a section to the speakers section - done
+  // 3.display the sample display in inner html - done
+  // 4.loop through objects - done
 
 const artistInfo = [
   {
     name: 'Metallica',
     photo: 'images/james.png',
-    story: 'Lorem ipsum dolor sit amet.', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
+    story: ' story is: Lorem ipsum.', 
+    description: ' description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
   },
   {
     name: 'Megadeth',
-    photo: 'images/james.png',
-    story: 'Lorem ipsum dolor sit amet.', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
+    photo: 'images/megadeth.png',
+    story: ' story is: Lorem ipsum.', 
+    description: ' description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
  },
 
   {
     name: 'Black Sabbath',
     photo: 'images/ozzy.png',
-    story: 'Lorem ipsum dolor sit amet.', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
+    story: ' story is: Lorem ipsum.', 
+    description: ' description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
   },
 
   {
     name: 'Rolling Stones',
     photo: 'images/mickjagger.png',
-    story: 'Lorem ipsum dolor sit amet.', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
+    story: ' story is: Lorem ipsum.', 
+    description: ' description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
   },
 
   {
     name: 'Foo Fighters',
-    photo: 'www.callingallgigs.png',
-    story: 'Lorem ipsum dolor sit amet.', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
+    photo: 'images/www.callingallgigs.png',
+    story: ' story is: Lorem ipsum.', 
+    description: ' description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
   },
 
   {
     name: 'AC/DC',
     photo: 'images/acdc.png',
-    story: 'Lorem ipsum dolor sit amet.', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
+    story: ' story is:Lorem ipsum.', 
+    description: ' description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Id cum, quaerat fugit vel magni modi.'
   }
 ]
 
+const artist = document.getElementById('artists')
+
 for (let i = 0; i < artistInfo.length; i++) {
-  
+  artist.innerHTML += `
+<div class="artist">
+<img class="artist_photo" src= ${artistInfo[i].photo} alt="metallica logo">
+<div class="artist_info">
+    <h3 class="artist_name">${artistInfo[i].name}</h3>
+    <p class="artist_story">${artistInfo[i].name}${artistInfo[i].story}</p>
+    <div class="line_left"></div>
+    <p class="artist_description">${artistInfo[i].name}${artistInfo[i].description}</p>
+</div>
+</div>
+`
 }
+
+const artistTitle = document.createElement('h2');
+artistTitle.innerHTML = 'Featured artists'
+artist.prepend(artistTitle)
+
+
